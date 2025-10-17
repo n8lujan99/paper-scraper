4b295e7b68586559862f0f9a90197208a68f0fa3
n8lujan99 <128549537+n8lujan99@users.noreply.github.com>
2025-10-17 11:09:14 -0500

    Delete Draco_Dark_OSPM directory

--- Draco_Dark_OSPM/python/OSPM/__init__.py
+++
@@ -1,8 +0,0 @@
-# --- Import all core OSPM modules ---
-from .OSPM_Orca import *
-from .orbit_integration import *
-from .physics_models import *
-from .runtime_state import *
-from .orbit_readers_writers import *
-from .OSPM_Binning import *
-from .OSPM_Matrix import *
--- Draco_Dark_OSPM/python/fortran/ospm_mods/archive/Core_comps/energy.f
+++
@@ -1,1 +0,0 @@
-<contents of energy.f>
